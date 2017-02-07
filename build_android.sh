e57ca11ee1d72264214e820328bfe5a2f737d72c
Yunxing Dai <yunxing@fb.com>
2017-02-07 11:52:28 -0800

    Remove container after commit

--- build_android.sh
+++ build_android.sh
@@ -2,11 +2,11 @@
 set -e
 mkdir -p _dist
 echo "Building armv7"
-docker run -v $(pwd):/workspace -it yunxing/reason-cross-compile-armv7-32 make android-armv7
+docker run -v $(pwd):/workspace --rm -it yunxing/reason-cross-compile-armv7-32 make android-armv7
 cp _build/librelayout.a _dist/librelayout-armv7.a
 echo "Artifact stored at _dist/relayout-armv7.a"
 
 echo "Building x86"
-docker run -v $(pwd):/workspace -it yunxing/reason-cross-compile-x86-32 make android-x86
+docker run -v $(pwd):/workspace --rm -it yunxing/reason-cross-compile-x86-32 make android-x86
 cp _build/librelayout.a _dist/librelayout-x86.a
 echo "Artifact stored at _dist/relayout-x86.a"
