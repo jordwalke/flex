61062e2ea063bbdd9088d1e3887825711af6ddd5
Jordan <jordojw@gmail.com>
2020-03-03 00:44:59 -0800

    add release scripts for ease of publishing

---
+++ scripts/test-ci.cmd
@@ -0,0 +1,7 @@
+:; set -e
+
+:; # This will be the location of this script.
+:; DIR="$(cd "$(dirname "$0")" && pwd)"
+
+:; # Run ReasonNativeTests.exe with correct root set.
+:; REASON_NATIVE_ROOT="$DIR/../" esy x "TestCi.exe" "$@"
